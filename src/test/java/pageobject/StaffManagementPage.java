633e800201da5cb6a1750a6640725b40897968cd
nujaijey <798985817@qq.com>
2020-12-17 20:02:36 +0800

    update StaffManagement

--- src/test/java/pageobject/StaffManagementPage.java
+++ src/test/java/pageobject/StaffManagementPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.testng.log4testng.Logger;
 
 public class StaffManagementPage extends BasePage {
+    private By searchInputBy = By.xpath("//input[@placeholder='请输入工号/姓名/手机号其中一项']");
+    private By searchButtonBy = By.xpath("//span[text()='搜索']");
     private By addStaff = By.xpath("//span[text()='添加员工']");
 
     private WebDriver driver;
@@ -14,3 +16,4 @@ public class StaffManagementPage extends BasePage {
         super(driver);
     }
 }
+
